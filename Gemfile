4e635853f0b59406b26d6008c18d9ed9c22723cc
fuzza <alex.fuzza@gmail.com>
2016-11-01 15:09:27 +0200

    Fix gitignore, add fastlane, update Travis config

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'cocoapods', '~> 1.0.1'
+gem 'fastlane', '~> 1.106.0'
 gem 'jazzy', '~> 0.7'
--- Pods/Target Support Files/Pods-FLUXTests/Pods-FLUXTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FLUXTests : NSObject
-@end
-@implementation PodsDummy_Pods_FLUXTests
-@end
